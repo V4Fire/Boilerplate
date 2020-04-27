59bc28814570b63ea8c8b4b4ec5e6dd617498448
kobezzza <kobezzza@gmail.com>
2020-04-27 16:49:39 +0300

    :up:

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,9 @@
+/*!
+ * V4Fire Boilerplate
+ * https://github.com/V4Fire/Boilerplate
+ *
+ * Released under the MIT license
+ * https://github.com/V4Fire/Boilerplate/blob/master/LICENSE
+ */
+
 /// <reference types="@v4fire/client"/>
